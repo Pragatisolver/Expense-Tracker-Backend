5f192b201c6a8bc55ddcb05cb4093b3e6ff30739
Pragatisolver <pragatigodara30@gmail.com>
2026-01-01 12:38:49 +0530

    Prepare project for deployment

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/api/expenses/"
 LOGOUT_REDIRECT_URL = "/api-auth/login/"
+import os
+
+ALLOWED_HOSTS = ["*"]  # safe for demo projects
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
