00818e8e9a3e13cbf1df599b445a0b2c65d5afd8
Pragatisolver <pragatigodara30@gmail.com>
2026-01-30 17:38:14 +0530

    deploy

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Expense Tracker Backend is running")
+
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("expenses.urls")),
     path("api-auth/", include("rest_framework.urls")),
